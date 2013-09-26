1d3c779e756ff830968e4308d04e7fdcd8c627d9
resteve <resteve@zikzakmedia.com>
2013-09-26 17:31:47 +0200

    setup

--- setup.py
+++ setup.py
@@ -1,11 +1,19 @@
 #!/usr/bin/env python
-# This file is part of account_payment_es_csb_58 module for Tryton.
-# The COPYRIGHT file at the top level of this repository contains
-# the full copyright notices and license terms.
+#This file is part account_payment_es_csb_58 module for Tryton.
+#The COPYRIGHT file at the top level of this repository contains
+#the full copyright notices and license terms.
 
 from setuptools import setup
 import re
+import os
 import ConfigParser
+
+MODULE = 'account_payment_es_csb_58'
+PREFIX = 'trytonspain'
+MODULE2PREFIX = {}
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 config = ConfigParser.ConfigParser()
 config.readfp(open('tryton.cfg'))
@@ -20,13 +28,17 @@ minor_version = int(minor_version)
 requires = []
 for dep in info.get('depends', []):
     if not re.match(r'(ir|res|webdav)(\W|$)', dep):
-        requires.append('trytond_%s >= %s.%s, < %s.%s' %
-                (dep, major_version, minor_version, major_version,
-                    minor_version + 1))
+        prefix = MODULE2PREFIX.get(dep, 'trytond')
+        requires.append('%s_%s >= %s.%s, < %s.%s' %
+                (prefix, dep, major_version, minor_version,
+                major_version, minor_version + 1))
 requires.append('trytond >= %s.%s, < %s.%s' %
         (major_version, minor_version, major_version, minor_version + 1))
 
-setup(name='trytonspain_account_payment_es_csb_58',
+tests_require = ['proteus >= %s.%s, < %s.%s' %
+    (major_version, minor_version, major_version, minor_version + 1)]
+
+setup(name='%s_%s' % (PREFIX, MODULE),
     version=info.get('version', '0.0.1'),
     description=('Manages credits communicated by computer file to payment '
         'and collection management according to the norm 58 of the Spanish '
@@ -34,16 +46,15 @@ setup(name='trytonspain_account_payment_es_csb_58',
     author='Zikzakmedia SL',
     author_email='zikzak@zikzakmedia.com',
     url='http://www.zikzakmedia.com',
-    download_url=("https://bitbucket.org/trytonspain/"
-        "trytond-account_payment_es_csb_58"),
-    package_dir={'trytond.modules.account_payment_es_csb_58': '.'},
+    download_url="https://bitbucket.org/trytonspain/trytond-account_payment_es_csb_58",
+    package_dir={'trytond.modules.%s' % MODULE: '.'},
     packages=[
-        'trytond.modules.account_payment_es_csb_58',
-        'trytond.modules.account_payment_es_csb_58.tests',
+        'trytond.modules.%s' % MODULE,
+        'trytond.modules.%s.tests' % MODULE,
         ],
     package_data={
-        'trytond.modules.account_payment_es_csb_58': info.get('xml', []) \
-            + ['tryton.cfg', 'locale/*.po'],
+        'trytond.modules.%s' % MODULE: (info.get('xml', [])
+            + ['tryton.cfg', 'view/*.xml', 'locale/*.po', ]),
         },
     classifiers=[
         'Development Status :: 5 - Production/Stable',
@@ -66,8 +77,8 @@ setup(name='trytonspain_account_payment_es_csb_58',
     zip_safe=False,
     entry_points="""
     [trytond.modules]
-    account_payment_es_csb_58 = trytond.modules.account_payment_es_csb_58
-    """,
+    %s = trytond.modules.%s
+    """ % (MODULE, MODULE),
     test_suite='tests',
     test_loader='trytond.test_loader:Loader',
 )
