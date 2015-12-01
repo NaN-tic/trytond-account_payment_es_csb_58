c1a5195dff569d75b5922d1859b26424dc4f98d0
resteve <resteve@zikzakmedia.com>
2015-12-01 11:00:50 +0100

    Tests

--- tests/test_account_payment_es_csb_58.py
+++ tests/test_account_payment_es_csb_58.py
@@ -1,29 +1,18 @@
-#!/usr/bin/env python
 # This file is part of the account_payment_es_csb_58 module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
-from trytond.tests.test_tryton import test_view, test_depends
-import trytond.tests.test_tryton
 import unittest
+import trytond.tests.test_tryton
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class AccountPaymentEsCSB58TestCase(unittest.TestCase):
-    '''Test Account Payment ES CSB 58 module'''
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('account_payment_es_csb_58')
-
-    def test0005views(self):
-        'Test views'
-        test_view('account_payment_es_csb_58')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+class AccountPaymentEsCsb58TestCase(ModuleTestCase):
+    'Test Account Payment Es Csb 58 module'
+    module = 'account_payment_es_csb_58'
 
 
 def suite():
     suite = trytond.tests.test_tryton.suite()
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
-        AccountPaymentEsCSB58TestCase))
+        AccountPaymentEsCsb58TestCase))
     return suite
