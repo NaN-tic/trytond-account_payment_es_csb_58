c2dc520d3b414f0d5c97cac892ac2b312ad45fb5
Bernat Brunet Torruella <bernat@nan-tic.com>
2017-03-21 13:21:22 +0100

    Fix problems on create group and add acount_payment_sepa depends to use the same account fields and not need to create one

--- payment.py
+++ payment.py
@@ -2,10 +2,11 @@
 # This file is part of account_payment_es_csb_58 module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
-from trytond.pool import PoolMeta
+from trytond.pool import PoolMeta, Pool
 from trytond.model import fields
 from trytond.pyson import Eval
 import logging
+import banknumber
 
 try:
     from retrofix import c58
@@ -18,13 +19,82 @@ except ImportError:
     raise Exception(message)
 
 __all__ = [
+    'BankAccount',
     'Journal',
     'Group',
     ]
-__metaclass__ = PoolMeta
+
+province = {
+    'none': '',
+    'ES-VI': '01',
+    'ES-AB': '02',
+    'ES-A': '03',
+    'ES-AL': '04',
+    'ES-AV': '05',
+    'ES-BA': '06',
+    'ES-PM': '07',
+    'ES-B': '08',
+    'ES-BU': '09',
+    'ES-CC': '10',
+    'ES-CA': '11',
+    'ES-CS': '12',
+    'ES-CR': '13',
+    'ES-CO': '14',
+    'ES-C': '15',
+    'ES-CU': '16',
+    'ES-GI': '17',
+    'ES-GR': '18',
+    'ES-GU': '19',
+    'ES-SS': '20',
+    'ES-H': '21',
+    'ES-HU': '22',
+    'ES-J': '23',
+    'ES-LE': '24',
+    'ES-L': '25',
+    'ES-LO': '26',
+    'ES-LU': '27',
+    'ES-M': '28',
+    'ES-MA': '29',
+    'ES-MU': '30',
+    'ES-NA': '31',
+    'ES-OR': '32',
+    'ES-O': '33',
+    'ES-P': '34',
+    'ES-GC': '35',
+    'ES-PO': '36',
+    'ES-SA': '37',
+    'ES-TF': '38',
+    'ES-S': '39',
+    'ES-SG': '40',
+    'ES-SE': '41',
+    'ES-SO': '42',
+    'ES-T': '43',
+    'ES-TE': '44',
+    'ES-TO': '45',
+    'ES-V': '46',
+    'ES-VA': '47',
+    'ES-BI': '48',
+}
+
+
+class BankAccount:
+    __metaclass__ = PoolMeta
+    __name__ = 'bank.account'
+
+    def get_first_other_number(self):
+        iban = None
+        for number in self.numbers:
+            if number.type == 'other':
+                return number.number
+            elif not iban and number.type == 'iban':
+                iban = number.number
+        if iban:
+            return iban[4:].replace(' ', '')
+        return None
 
 
 class Journal:
+    __metaclass__ = PoolMeta
     __name__ = 'account.payment.journal'
     csb58_include_domicile = fields.Boolean('Include Domicile')
 
@@ -35,6 +105,20 @@ class Journal:
             cls.process_method.selection.extend([
                     ('csb58', 'CSB 58'),
                     ])
+
+        if 'invisible' not in cls.sepa_bank_account_number.states:
+            cls.sepa_bank_account_number.states['invisible'] = (
+                Eval('process_method') != 'csb58')
+        else:
+            cls.sepa_bank_account_number.states['invisible'] &= (
+                Eval('process_method') != 'csb58')
+
+        if 'required' not in cls.sepa_bank_account_number.states:
+            cls.sepa_bank_account_number.states['required'] = (
+                Eval('process_method') == 'csb58')
+        else:
+            cls.sepa_bank_account_number.states['required'] |= (
+                Eval('process_method') == 'csb58')
 
     @staticmethod
     def default_csb58_include_domicile():
@@ -49,10 +133,233 @@ class Journal:
 
 
 class Group:
+    __metaclass__ = PoolMeta
     __name__ = 'account.payment.group'
 
+    @classmethod
+    def __setup__(cls):
+        super(Group, cls).__setup__()
+        cls._error_messages.update({
+                'company_without_complete_address': ('The company %s has no a '
+                    'complete address to add to the file.'),
+                'party_without_address': ('The party %s has no any address to '
+                    'add to the file'),
+                'party_without_complete_address': ('The party %s has no a '
+                    'complete address to build the file.'),
+                'party_without_province': ('The party %s has no any province '
+                    'assigned at its address'),
+                'party_without_vat_number': ('The party %s has no any vat '
+                    'number.'),
+                'no_lines': ('Can not generate export file, there are not '
+                    'payment lines.'),
+                'bank_account_not_defined': ('The bank account of the company '
+                    '%s is not defined.'),
+                'wrong_bank_account': ('The bank account number of the '
+                    'company %s is not correct.'),
+                'vat_number_not_defined': ('The company have not any VAT '
+                    'number defined.'),
+                'customer_bank_account_not_defined': ('The bank account '
+                    'number of the party %s is not defined and current payment'
+                    ' journal enforces all lines to have a bank account.'),
+                'wrong_party_bank_account': ('The bank account number of the '
+                    'party %s is not correct.'),
+                'wrong_payment_journal': ('The payment journal has no norm to '
+                    'build a file.'),
+                'unknown_error': ('Unknown error. An error occurred creating '
+                    'the file.'),
+                'remittance': 'remittance',
+                'configuration_error': ('Configuration Error!'),
+                'payment_without_bank_account': (
+                    'The payment "%s" doesn\'t have bank account.'),
+                'party_without_bank_account': (
+                    'The party "%s" doesn\'t have bank account.'),
+                })
+
     def set_default_csb58_payment_values(self):
-        values = self.set_default_payment_values()
+        pool = Pool()
+        Party = pool.get('party.party')
+        Date = pool.get('ir.date')
+        today = Date.today()
+        values = {}
+        journal = self.journal
+        values['payment_journal'] = journal
+        values['party'] = journal.party
+        values['name'] = values['party'].name
+
+        # Checks bank account code.
+        bank_account = journal.sepa_bank_account_number.account
+        if not bank_account:
+            self.raise_user_error('configuration_error',
+                error_description='bank_account_not_defined',
+                error_description_args=(values['name']))
+        code = bank_account.get_first_other_number()
+        if not code or not banknumber.check_code('ES', code):
+            self.raise_user_error('configuration_error',
+                        error_description='wrong_bank_account',
+                        error_description_args=(values['name'],))
+
+        # Checks vat number
+        vat = journal.party and journal.party.vat_number \
+            or False
+        if not vat:
+            self.raise_user_error('configuration_error',
+                        error_description='vat_number_not_defined',
+                        error_description_args=(values['name']))
+
+        # Checks whether exists lines
+        payments = self.payments
+        if not payments:
+            self.raise_user_error('no_lines')
+
+        values['number'] = str(self.id)
+        values['payment_date'] = self.planned_date if self.planned_date \
+            else today
+        values['creation_date'] = today
+        values['vat_number'] = vat
+        values['suffix'] = journal.suffix
+        values['company_name'] = journal.company.party.name
+        values['bank_account'] = code
+        values['ine_code'] = journal.ine_code
+        values['amount'] = 0
+
+        values['address'] = Party.address_get(values['party'], type='invoice')
+        if values['address']:
+            values['street'] = values['address'].street
+            values['zip'] = values['address'].zip
+            values['city'] = values['address'].city
+            values['subdivision'] = values['address'].subdivision or False
+            values['province'] = province[values['subdivision'].code
+                    if (values['subdivision']
+                        and values['subdivision'].type == 'province')
+                    else 'none']
+
+        receipts = []
+        if self.join:
+            parties_bank_accounts = {}
+            # Join all receipts of the same party and bank_account
+            for payment in payments:
+                key = (payment.party, payment.bank_account)
+                if key not in parties_bank_accounts:
+                    parties_bank_accounts[key] = [payment]
+                else:
+                    parties_bank_accounts[key].append(payment)
+            for party_bank_account in parties_bank_accounts:
+                if not party_bank_account or not party_bank_account[1]:
+                    self.raise_user_error('party_without_bank_account',
+                        party_bank_account and party_bank_account[0]
+                            and party_bank_account[0].rec_name)
+                amount = 0
+                communication = ''
+                date = False
+                maturity_date = today
+                create_date = False
+                date_created = False
+                invoices = []
+                for payment in parties_bank_accounts[party_bank_account]:
+                    amount += payment.amount
+                    communication += '%s %s' % (payment.id,
+                        payment.description)
+                    if not date or date < payment.date:
+                        date = payment.date
+                    if payment.line:
+                        if (not maturity_date
+                                or maturity_date < payment.line.maturity_date):
+                            maturity_date = payment.line.maturity_date
+                        invoices.append(payment.line.origin)
+                    if not create_date or create_date < payment.create_date:
+                        create_date = payment.create_date
+                    if not date_created or date_created < payment.date:
+                        date_created = payment.date
+
+                vals = {
+                    'party': party_bank_account[0],
+                    'bank_account':
+                        party_bank_account[1].get_first_other_number(),
+                    'invoices': invoices,
+                    'amount': amount,
+                    'communication': communication,
+                    'date': date,
+                    'maturity_date': maturity_date,
+                    'create_date': create_date,
+                    'date_created': date_created,
+                    'vat_number': party_bank_account[0].vat_number,
+                    }
+                address = Party.address_get(party_bank_account[0],
+                    type='invoice')
+                if address:
+                    vals['name'] = vals['party'].name
+                    vals['address'] = address
+                    vals['street'] = address.street or False
+                    vals['streetbis'] = address.streetbis or False
+                    vals['zip'] = address.zip or False
+                    vals['city'] = address.city or False
+                    vals['country'] = address.country or False
+                    if vals['country']:
+                        vals['country_code'] = (vals['country'].code
+                            or False)
+                    vals['subdivision'] = address.subdivision or False
+                    vals['state'] = (vals['subdivision']
+                            and vals['subdivision'].name or '')
+                    vals['province'] = province[vals['subdivision'].code
+                            if (vals['subdivision']
+                                and vals['subdivision'].type == 'province')
+                            else 'none']
+                receipts.append(vals)
+                values['amount'] += abs(amount)
+        else:
+            # Each payment is a receipt
+            for payment in payments:
+                if not payment.bank_account:
+                    self.raise_user_error('payment_without_bank_account',
+                        payment.rec_name)
+                party = payment.party
+                amount = payment.amount
+                vals = {
+                    'party': party,
+                    'bank_account':
+                        payment.bank_account.get_first_other_number(),
+                    'invoices': [payment.line and payment.line.origin or None],
+                    'amount': amount,
+                    'communication': '%s %s' % (payment.id,
+                        payment.description),
+                    'date': payment.date,
+                    'maturity_date': (payment.line
+                        and payment.line.maturity_date or today),
+                    'create_date': payment.create_date,
+                    'date_created': payment.date,
+                    'vat_number': party.vat_number,
+                    }
+                address = Party.address_get(party, type='invoice')
+                if address:
+                    vals['name'] = vals['party'].name
+                    vals['address'] = address
+                    vals['street'] = address.street or False
+                    vals['streetbis'] = address.streetbis or False
+                    vals['zip'] = address.zip or False
+                    vals['city'] = address.city or False
+                    vals['country'] = address.country or False
+                    if vals['country']:
+                        vals['country_code'] = vals['country'].code or False
+                    vals['subdivision'] = address.subdivision or False
+                    if vals['subdivision']:
+                        vals['state'] = vals['subdivision'].name or ''
+                    vals['province'] = province[vals['subdivision'].code
+                            if (vals['subdivision']
+                                and vals['subdivision'].type == 'province')
+                            else 'none']
+                receipts.append(vals)
+                values['amount'] += abs(amount)
+        if journal.require_bank_account:
+            for receipt in receipts:
+                if not receipt['bank_account']:
+                    self.raise_user_error('configuration_error',
+                        error_description='customer_bank_account_not_defined',
+                        error_description_args=(receipt['name'],))
+                if not banknumber.check_code('ES', receipt['bank_account']):
+                    self.raise_user_error('configuration_error',
+                        error_description='wrong_party_bank_account',
+                        error_description_args=(receipt['name'],))
+        values['receipts'] = receipts
         values['include_domicile'] = values['payment_journal'].\
             csb58_include_domicile
         values['record_count'] = 0
@@ -60,7 +367,6 @@ class Group:
         values['ordering_record_count'] = 0
         for receipt in values['receipts']:
             receipt['reference'] = receipt['party'].code
-
             if not receipt['address']:
                 self.raise_user_error('configuration_error',
                     error_description='party_without_address',
@@ -71,6 +377,19 @@ class Group:
                     error_description='party_without_complete_address',
                     error_description_args=(receipt['party'].name,))
         return values
+
+    def attach_file(self, data):
+        IrAttachment = Pool().get('ir.attachment')
+        journal = self.journal
+        values = {
+            'name': '%s_%s_%s' % (
+                self.raise_user_error('remittance', raise_exception=False),
+                journal.process_method, self.reference),
+            'type': 'data',
+            'data': data,
+            'resource': '%s' % (self),
+            }
+        IrAttachment.create([values])
 
     @classmethod
     def process_csb58(cls, group):
